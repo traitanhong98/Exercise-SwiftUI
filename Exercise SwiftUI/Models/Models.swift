ec9651f2a0b63d540b5bac81bd0464d566993231
An <annx2812@gmail.com>
2022-04-17 22:29:10 +0700

    first commit

---
+++ Exercise SwiftUI/Models/Models.swift
@@ -0,0 +1,19 @@
+//
+//  Models.swift
+//  Exercise
+//
+//  Created by Nguyễn Hoàng on 17/04/2022.
+//
+
+import Foundation
+
+struct Employee {
+    var id: String
+    var name: String
+    var createAt: String
+    var avatar: String
+    var address: String
+    var isMale: Bool
+    var status: String
+    var dayWorking: Int
+}
