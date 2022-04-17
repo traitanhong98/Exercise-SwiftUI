d597dfb774a8bfae1d43d36384b3cd12f18ffd53
An <annx2812@gmail.com>
2022-04-17 22:27:10 +0700

    Initial Commit

---
+++ Exercise SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Exercise SwiftUI
+//
+//  Created by Nguyễn Hoàng on 17/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Exercise SwiftUI/Exercise_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise_SwiftUIApp.swift
+//  Exercise SwiftUI
+//
+//  Created by Nguyễn Hoàng on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Exercise_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
