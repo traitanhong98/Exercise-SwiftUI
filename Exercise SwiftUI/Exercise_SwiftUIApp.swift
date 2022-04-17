c4901e77e46bccc1fb952bcdac3e5f3d1e531da4
An <annx2812@gmail.com>
2022-04-17 22:29:37 +0700

    Add datas

--- Exercise SwiftUI/Exercise_SwiftUIApp.swift
+++ Exercise SwiftUI/Exercise_SwiftUIApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct Exercise_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
+            .navigationBarHidden(true)
         }
     }
 }
